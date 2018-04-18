b7b2dd3b84fc7278a8b2f8cded02c7aeb277debc
tayutaedomo <tayutaedomo@gmail.com>
2018-04-18 20:35:29 +0900

    Add project files

---
+++ ConsoleTrialLinq/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleTrialLinq
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = new[]
+            {
+                new { X = 0, Y = 10, Z = new[]{ 1, 2, 3} },
+                new { X = 1, Y = 11, Z = new[]{ 4, 5, 6} },
+                new { X = 2, Y = 12, Z = new[]{ 7, 8, 9} },
+                new { X = 3, Y = 13, Z = new[]{ 0, 1, 2} },
+                new { X = 4, Y = 14, Z = new[]{ 3, 4, 5} },
+            };
+
+            {
+                var b =
+                    from p in a
+                    select p.X;
+                //var b = a.Select(p => p.X);
+                foreach (var p in b)
+                    Console.Write("{0} ", p);
+            }
+
+            Console.WriteLine("");
+
+            {
+                var b = a.Select(p => p.X);
+                foreach (var p in b)
+                    Console.Write("{0} ", p);
+            }
+        }
+    }
+}
---
+++ ConsoleTrialLinq/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleTrialLinq")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleTrialLinq")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("B1A9EF0A-0DA2-4FB3-B10D-69C1AB11391B")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
