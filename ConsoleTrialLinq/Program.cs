cb62df900c7a6f8de0575bb3b21f5ec23746f691
tayutaedomo <tayutaedomo@gmail.com>
2018-04-18 20:43:38 +0900

    Add let code

--- ConsoleTrialLinq/Program.cs
+++ ConsoleTrialLinq/Program.cs
@@ -7,13 +7,15 @@ namespace ConsoleTrialLinq
     {
         public static void Main(string[] args)
         {
+            // Refer: http://ufcpp.net/study/csharp/sp3_stdquery.html
+
             var a = new[]
             {
                 new { X = 0, Y = 10, Z = new[]{ 1, 2, 3} },
                 new { X = 1, Y = 11, Z = new[]{ 4, 5, 6} },
                 new { X = 2, Y = 12, Z = new[]{ 7, 8, 9} },
                 new { X = 3, Y = 13, Z = new[]{ 0, 1, 2} },
-                new { X = 4, Y = 14, Z = new[]{ 3, 4, 5} },
+                new { X = 4, Y = 14, Z = new[]{ 3, 4, 5} }
             };
 
             {
@@ -24,13 +26,30 @@ namespace ConsoleTrialLinq
                 foreach (var p in b)
                     Console.Write("{0} ", p);
             }
-
-            Console.WriteLine("");
-
             {
                 var b = a.Select(p => p.X);
                 foreach (var p in b)
                     Console.Write("{0} ", p);
+            }
+
+            Console.Write("\n\n");
+
+            {
+                var b =
+                    from p in a
+                    let sumZ = p.Z.Sum()
+                    select new { p.X, sumZ };
+
+                foreach (var p in b)
+                    Console.Write("{0}\n", p);
+            }
+            {
+                var b = a
+                    .Select(p => new { p, SumZ = p.Z.Sum() })
+                    .Select(p2 => new { p2.p.X, p2.SumZ });
+
+                foreach (var p in b)
+                    Console.Write("{0}\n", p);
             }
         }
     }
